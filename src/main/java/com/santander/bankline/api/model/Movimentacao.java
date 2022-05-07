841b739580150a64a9848836bcc26ee17be40f69
Mogleson Lima <moglesonlima@alu.ufc.br>
2022-05-07 16:39:48 -0300

    chore: Adding Bankline API project, already with the Model layer.

---
+++ src/main/java/com/santander/bankline/api/model/Movimentacao.java
@@ -0,0 +1,10 @@
+package com.santander.bankline.api.model;
+
+import java.time.LocalDateTime;
+
+public class Movimentacao {
+	private Integer id;
+	private LocalDateTime dataHora;
+	private String descricao;
+	private Double valor;
+}
